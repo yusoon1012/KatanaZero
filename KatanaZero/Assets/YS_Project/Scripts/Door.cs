0428105300853619adc494e37d7a39b0ff81d13c
DESKTOP-QCSI74L\mater <faramandle123@gmail.com>
2023-08-06 16:24:30 +0900

    <yusoon> feat: 플레이어가 문에 다가가면 문이 열리는 로직 추가

---
+++ KatanaZero/Assets/YS_Project/Scripts/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    BoxCollider2D doorCollider;
+    Animator doorAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorCollider=GetComponent<BoxCollider2D>();
+        doorAni=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doorAni.GetCurrentAnimatorStateInfo(0).IsName("DoorOpen")&&doorAni.GetCurrentAnimatorStateInfo(0).normalizedTime>=1.0f)
+        {
+            doorCollider.enabled=false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Equals("Player"))
+        {
+            
+            doorAni.SetTrigger("DoorOpen");
+        }
+    }
+}
